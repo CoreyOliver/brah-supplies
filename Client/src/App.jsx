44a7f0b09a753c0cca61d7e39e2fc991f8853b6d
CoreyOliver <coreyoliver.dev@gmail.com>
2023-06-07 15:27:04 -0400

    first commit: client init

---
+++ Client/src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <div className='flex items-center justify center'>
+      <h1 className='flex items-center'>Hello Errryone</h1>
+    </div>
+  )
+}
+
+export default App
