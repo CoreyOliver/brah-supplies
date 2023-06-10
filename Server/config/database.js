1ecd6f5c5304d49d2c3c917664de4c175e7950d2
CoreyOliver <coreyoliver.dev@gmail.com>
2023-06-10 12:28:28 -0400

    removed db error

--- Server/config/database.js
+++ Server/config/database.js
@@ -4,9 +4,7 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true
+      useUnifiedTopology: true
     })
 
     console.log(`MongoDB Connected: ${conn.connection.host}`)
